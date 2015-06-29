8ac38d70c1c5cc81b1dc0bf1402e245fb543155a
Adam Brodzinski <adambrodzinski@gmail.com>
2015-06-29 10:37:42 -0400

    partially working SSR
    React-Router throws error if you render the Router component.
    I think this is due to an error with module.exports?
    
    https://github.com/rackt/react-router/issues/924#issuecomment-113787665

--- server/routes.jsx
+++ server/routes.jsx
@@ -1,7 +1,27 @@
+var { Route, DefaultRoute } = Router;
+
 // match all routes (.*)
-Picker.route('/about', function(params, req, res, next) {
+Picker.route('/test', function(params, req, res, next) {
 
-  var location = '/about';//new Location(req.path, req.query);
-  res.end("Hello " + location);
+  var location = '/about';
 
+  var routes = (
+    <Route path="/" handler={Layout}>
+
+      <DefaultRoute name='home' handler={Home}/>
+      <Route name='about' path="about" handler={About}/>
+
+    </Route>
+  );
+
+  // this works if you uncomment and go to /test
+  //
+  //var html = React.renderToString(<Home />);
+  //return res.end(html);
+
+  Router.run(routes, location, function(error, initialState, transition) {
+    //var html = React.renderToString(React.createElement(Router, {}));
+    var html = React.renderToString(<Router {...initialState}/>);
+    res.end(html);
+  });
 });
